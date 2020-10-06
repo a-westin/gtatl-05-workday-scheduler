7de9955c2327521b77c951fe373ae05643b37164
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-10-06 18:53:40 -0400

    document.ready function, defined variables

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  // Defining variables
+  var timeblockContainer = $("timeblockContainer");
+  var workday = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+  var currentHour = moment().format("h");
+});
