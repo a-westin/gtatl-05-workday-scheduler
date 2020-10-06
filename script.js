e233daced6216a17e9096c584b47e2663759f364
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-10-06 19:55:15 -0400

    for loop to display timeblocks, if/else statment to determine past-present-future

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ $(document).ready(function () {
 
 // Populating the p tag with today's date
 $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+
+// For loop to create what will display on the calendar
+for (var i = 0; i < workday.length; i++) {
+  var timeBlock = $("<div>").addClass("row");
+  var hourBlock = $("<div>").addClass("hour");
+  var textArea = $("<textarea>");
+  textArea.text(localStorage.getItem(hours[i]));
+
+  if (currentHour == hours[i]) {
+    textArea.addClass("present");
+  } else if (currentHour < hours[i]) {
+    textArea.addClass("past");
+  } else if (currentHour > hours[i]) {
+    textArea.addClass("future");
+  }
+}
