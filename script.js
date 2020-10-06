e83293ea8ef5aa9a6a6d7de7493291de7c9439ef
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-10-06 19:01:11 -0400

    populating p tag w/ current date

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ $(document).ready(function () {
   var workday = [9, 10, 11, 12, 1, 2, 3, 4, 5];
   var currentHour = moment().format("h");
 });
+
+// Populating the p tag with today's date
+$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
