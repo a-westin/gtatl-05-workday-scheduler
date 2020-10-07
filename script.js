12d972d61f72fcce6361c8ce0c0455c44c4c8776
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-10-06 20:30:23 -0400

    forgot to link my script.js file, added save button

--- script.js
+++ script.js
@@ -3,23 +3,26 @@ $(document).ready(function () {
   var timeblockContainer = $("timeblockContainer");
   var workday = [9, 10, 11, 12, 1, 2, 3, 4, 5];
   var currentHour = moment().format("h");
-});
 
-// Populating the p tag with today's date
-$("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
+  // Populating the p tag with today's date
+  $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
-// For loop to create what will display on the calendar
-for (var i = 0; i < workday.length; i++) {
-  var timeBlock = $("<div>").addClass("row");
-  var hourBlock = $("<div>").addClass("hour");
-  var textArea = $("<textarea>");
-  textArea.text(localStorage.getItem(hours[i]));
+  // For loop to create what will display on the calendar
+  for (var i = 0; i < workday.length; i++) {
+    var timeBlock = $("<div>").addClass("row");
+    var hourBlock = $("<div>").addClass("hour");
+    var textArea = $("<textarea>");
+    textArea.text(localStorage.getItem(hours[i]));
 
-  if (currentHour == hours[i]) {
-    textArea.addClass("present");
-  } else if (currentHour < hours[i]) {
-    textArea.addClass("past");
-  } else if (currentHour > hours[i]) {
-    textArea.addClass("future");
+    if (currentHour == hours[i]) {
+      textArea.addClass("present");
+    } else if (currentHour < hours[i]) {
+      textArea.addClass("past");
+    } else if (currentHour > hours[i]) {
+      textArea.addClass("future");
+    }
+    var saveBtn = $("<button>").addClass("saveBtn");
+    timeBlock.append(hour, textArea, saveBtn);
+    timeblockContainer.append(timeBlock);
   }
-}
+});
